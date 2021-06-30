8e7ed62c0863c1b6b313737cb318cc26d5e9bf83
inoomene <imen.nmn@gmail.com>
2021-06-30 08:37:17 +0200

    Remove unused fuction and reformat percent

--- app/src/main/java/com/inoomene/fizzbuzz/utils/UtilExtension.kt
+++ app/src/main/java/com/inoomene/fizzbuzz/utils/UtilExtension.kt
@@ -1,28 +1,15 @@
 package com.inoomene.fizzbuzz.utils
 
 import android.text.*
-import android.text.style.ForegroundColorSpan
-import androidx.annotation.ColorInt
 import androidx.core.text.HtmlCompat
-
-fun String.toSpannableString(@ColorInt colorInt : Int): SpannableString{
-    val spannableString = SpannableString(this)
-    val foregroundSpan = ForegroundColorSpan(colorInt)
-    spannableString.setSpan(
-        foregroundSpan,
-        0,
-        spannableString.length,
-        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-    )
-    return spannableString
-}
 
  fun String.toColoredSpanned(color: String): String {
     return "<font color=$color>$this</font>"
 }
 
-fun Int.percent(total: Int) : Int{
-    return this*100/total
+fun Int.percent(total: Int) : String{
+    val percent = toDouble()*100/total
+    return String.format("%.1f", percent)
 }
 
  fun CharSequence.add(other: String): CharSequence {
